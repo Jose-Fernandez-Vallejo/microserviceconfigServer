80a1c3cce209df24f1cfaf595a836b365321486f
JoseFernandez <josejafv1997@gmail.com>
2021-11-15 12:47:32 +0100

    add config

--- microserviceConfigServer/src/test/java/com/example/microserviceConfigServer/MicroserviceConfigServerApplicationTests.java
+++ microserviceConfigServer/src/test/java/com/example/microserviceConfigServer/MicroserviceConfigServerApplicationTests.java
@@ -1,12 +1,9 @@
 package com.example.microserviceConfigServer;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MicroserviceConfigServerApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
