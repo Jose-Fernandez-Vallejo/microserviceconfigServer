9bae2ed051f2fd283595725ae4cbdd098920e25e
JoseFernandez <josejafv1997@gmail.com>
2021-11-15 11:30:46 +0100

    añador proyecto

---
+++ microserviceConfigServer/src/test/java/com/example/microserviceConfigServer/MicroserviceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microserviceConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
